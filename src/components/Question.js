dca709e0ecf3a85d3d829801234c5f1b18586253
Elise Erickson <elisece717@gmail.com>
2023-12-31 16:22:24 -0500

    satisfies tests

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,7 +3,17 @@ import React, { useState } from "react";
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  React.useEffect( () => {
+    const questionTimer = setTimeout( () => {
+      if(timeRemaining === 1){
+        onAnswered(false)
+      }
+      setTimeRemaining(timeRemaining - 1)
+    }, 1000)
+    return function cleanup(){
+      clearTimeout(questionTimer)
+    }
+  }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
